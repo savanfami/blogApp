2c996ea438c9d5bd6821ea5043f11d5d29c1e3b0
Savan Fami <savanfami1239@gmail.com>
2025-03-11 14:48:20 +0530

    fixed bug

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Blog App</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+  <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
   @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
